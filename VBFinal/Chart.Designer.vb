dfcbada636dd35edc8e97a09d8e243926b851833
António Estevam <antonio.estevam@ondalogica.com>
2022-02-17 17:22:58 +0000

    Add project files.

---
+++ VBFinal/Chart.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Chart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.PictureBox1.Image = Global.VBFinal.My.Resources.Resources.noData
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 29)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1008, 521)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "csv files (*.csv)|*.csv"
+        Me.OpenFileDialog1.RestoreDirectory = True
+        Me.OpenFileDialog1.ShowReadOnly = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Load data"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(975, 32)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(22, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "➕"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Button3.Location = New System.Drawing.Point(947, 32)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(22, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "➖"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.HScrollBar1.LargeChange = 2
+        Me.HScrollBar1.Location = New System.Drawing.Point(0, 575)
+        Me.HScrollBar1.Maximum = 1
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(1008, 17)
+        Me.HScrollBar1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(1016, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "NA"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(1016, 533)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "NA"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox1.ForeColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(3, 32)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(45, 19)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "MA"
+        Me.CheckBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(1016, 282)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(27, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "NA"
+        '
+        'Chart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1210, 592)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Chart"
+        Me.Text = "Chart"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ VBFinal/Chart.vb
@@ -0,0 +1,241 @@
+﻿Imports System.IO
+
+Public Class Chart
+    Dim bitmap As Bitmap
+    Dim entries As List(Of Entry) = New List(Of Entry)
+    Dim MAs As List(Of Double) = New List(Of Double)
+    Dim max As Double
+    Dim min As Double
+    Dim width As Double
+    Dim MAValue As Integer = 0
+    Dim startIndex As Integer = 0
+    Dim range As Integer = 0
+
+    Private Sub LoadData(sender As Object, e As EventArgs) Handles Button1.Click
+        entries.Clear()
+        If OpenFileDialog1.ShowDialog() = DialogResult.Cancel Then
+            Return
+        End If
+
+        Dim csvReader As New StreamReader(OpenFileDialog1.OpenFile)
+        Do While csvReader.Peek() <> -1
+            Dim _entry As String() = csvReader.ReadLine().Split(",")
+            Try
+                Dim timeStamp As DateTime = DateTime.Parse(_entry(0))
+                Dim open As Double = Double.Parse(_entry(1).Replace(".", ","))
+                Dim high As Double = Double.Parse(_entry(2).Replace(".", ","))
+                Dim low As Double = Double.Parse(_entry(3).Replace(".", ","))
+                Dim close As Double = Double.Parse(_entry(4).Replace(".", ","))
+                entries.Add(New Entry(timeStamp, open, high, low, close))
+            Catch ex As Exception
+                Continue Do
+            End Try
+        Loop
+
+        For counter As Integer = 0 To entries.Count - 20
+            MAs.Add((From x In entries Select x.AVG Skip counter Take 20).Average)
+        Next
+        'entries.Reverse()
+
+        range = 50 'entries.Count
+        HScrollBar1.Maximum = entries.Count - 1
+        HScrollBar1.LargeChange = range
+        width = PictureBox1.Width / range
+        max = (From x In entries Select x.high).Max
+        min = (From x In entries Select x.low).Min
+
+        Label1.Text = max
+        Label2.Text = min
+        Label3.Text = (max - min) / 2 + min
+
+        RenderChart()
+    End Sub
+
+    Private Sub Chart_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        bitmap = New Bitmap(PictureBox1.Width, PictureBox1.Height, Imaging.PixelFormat.Format32bppArgb)
+        'If entries.Count = 0 Then
+        '    Return
+        'End If
+        width = PictureBox1.Width / range
+        RenderChart()
+    End Sub
+
+    Private Sub ZoomOut(sender As Object, e As EventArgs) Handles Button3.Click
+        Zoom("out")
+    End Sub
+    Private Sub ZoomIn(sender As Object, e As EventArgs) Handles Button2.Click
+        Zoom("in")
+    End Sub
+    Sub Zoom(type As String)
+        If type = "in" Then
+            If entries.Count = 0 Or range - 10 < 0 Then
+                Return
+            End If
+            range -= 10
+        ElseIf type = "out" Then
+            If entries.Count = 0 Or range + 10 > entries.Count Then
+                Return
+            End If
+            range += 10
+        End If
+
+        HScrollBar1.Maximum = entries.Count - 1
+        HScrollBar1.LargeChange = range
+
+        width = PictureBox1.Width / range
+        max = (From x In entries Select x.high Skip startIndex Take range).Max
+        min = (From x In entries Select x.low Skip startIndex Take range).Min
+
+        Label1.Text = max
+        Label2.Text = min
+        Label3.Text = (max - min) / 2 + min
+
+        RenderChart()
+    End Sub
+
+    Private Sub Scroll(sender As Object, e As EventArgs) Handles HScrollBar1.ValueChanged
+        'If entries.Count = 0 Then
+        '    Return
+        'End If
+        startIndex = HScrollBar1.Value
+
+        'width = PictureBox1.Width / range
+        max = (From x In entries Select x.high Skip startIndex Take range).Max
+        min = (From x In entries Select x.low Skip startIndex Take range).Min
+
+        'Label1.Text = max
+        'Label2.Text = min
+        'Label3.Text = (max - min) / 2 + min
+
+        RenderChart()
+    End Sub
+
+    Private Sub DrawCandle(entry As Entry, index As Integer)
+        Dim _y As Double
+        Dim _height As Double
+        If entry.open < entry.close Then
+            _y = entry.open
+            _height = entry.close - entry.open
+        Else
+            _y = entry.close
+            _height = entry.open - entry.close
+        End If
+        Dim height As Double = (PictureBox1.Height * _height / (max - min))
+        Dim y As Double = ((_y - min) * PictureBox1.Height) / (max - min)
+        Dim color As Color = entry.color
+
+        'open/close
+        Dim g As Graphics = Graphics.FromImage(bitmap)
+        g.FillRectangle(New SolidBrush(color), New Rectangle(width * index + 1, y, width - 1, height))
+
+        'high/low
+        Dim hlX As Integer = width * index + (width / 2)
+        Dim hY As Integer = (entry.high - min) * PictureBox1.Height / (max - min)
+        Dim lY As Integer = (entry.low - min) * PictureBox1.Height / (max - min)
+        g.DrawLine(New Pen(color), hlX, lY, hlX, hY)
+    End Sub
+
+    Private Sub ToggleMA(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If MAValue = 0 Then
+            MAValue = 20
+        Else
+            MAValue = 0
+        End If
+
+        RenderChart()
+    End Sub
+    Sub DrawMA(index As Integer)
+        If index + startIndex >= MAValue And MAValue <> 0 Then
+            Dim g As Graphics = Graphics.FromImage(bitmap)
+            Dim sX As Integer = width * index
+            Dim sY As Integer = (MAs(index + startIndex - 20) - min) * PictureBox1.Height / (max - min)
+            Dim eX As Integer = width * (index + 1)
+            Dim eY As Integer = (MAs(index + startIndex - 20 + 1) - min) * PictureBox1.Height / (max - min)
+            g.DrawLine(New Pen(Color.Orange), sX, sY, eX, eY)
+        End If
+    End Sub
+
+    Sub Position(sP As Double, sPIndex As Integer, eP As Double, ePIndex As Integer)
+        Dim g As Graphics = Graphics.FromImage(bitmap)
+        Dim x1 As Integer = width * sPIndex + width / 2
+        Dim y1 As Integer = (sP - min) * PictureBox1.Height / (max - min)
+        Dim x2 As Integer = width * ePIndex + width / 2
+        Dim y2 As Integer = (eP - min) * PictureBox1.Height / (max - min)
+
+        Dim gainLoss As Int16 = (((eP - sP) + sP) / sP * 100) - 100
+
+        Dim color As Color
+        If sP < eP Then
+            color = Color.Green
+        ElseIf sP > eP Then
+            color = Color.Red
+        Else
+            color = Color.White
+        End If
+        g.DrawLine(New Pen(color, 2), x1, y1, x2, y1)
+        g.DrawLine(New Pen(color, 2), x2, y1, x2, y2)
+
+        g.DrawString(gainLoss.ToString + "%", Label1.Font, New SolidBrush(Color.White), New PointF(x1, y1))
+    End Sub
+
+    Sub RenderChart()
+        bitmap = New Bitmap(PictureBox1.Width, PictureBox1.Height, Imaging.PixelFormat.Format32bppArgb)
+
+        Dim Open As Boolean = False
+        Dim sP As Double
+        Dim sPIndex As Integer
+        Dim eP As Double
+        Dim ePIndex As Integer
+        For i As Integer = startIndex To startIndex + range - 1 'entries.Count - 1
+            DrawCandle(entries(i), i - startIndex)
+            DrawMA(i - startIndex)
+
+            If i >= 20 Then
+                If MAs(i - 20) < entries(i).close And Not Open Then
+                    'buy
+                    Open = True
+                    sP = MAs(i - startIndex + startIndex - 20)
+                    sPIndex = i - startIndex
+                ElseIf MAs(i - 20) > entries(i).close And Open Then
+                    'sell
+                    Open = False
+                    eP = MAs(i - startIndex + startIndex - 20)
+                    ePIndex = i - startIndex
+                End If
+            End If
+            If sP <> 0 And eP <> 0 Then
+                Position(sP, sPIndex, eP, ePIndex)
+                sP = 0
+                sPIndex = 0
+                eP = 0
+                sPIndex = 0
+            End If
+        Next
+        bitmap.RotateFlip(RotateFlipType.Rotate180FlipX)
+        PictureBox1.Image = bitmap
+    End Sub
+End Class
+
+Public Class Entry
+    Public open As Double
+    Public high As Double
+    Public low As Double
+    Public close As Double
+    Public timeStramp As DateTime
+    Public AVG As Double
+    Public color As Color
+
+    Public Sub New(timeStamp As DateTime, open As Double, high As Double, low As Double, close As Double)
+        Me.timeStramp = timeStamp
+        Me.open = open
+        Me.high = high
+        Me.low = low
+        Me.close = close
+        Me.AVG = (high + low) / 2
+        If open < close Then
+            Me.color = Color.FromArgb(38, 166, 154)
+        Else
+            Me.color = Color.FromArgb(239, 83, 80)
+        End If
+    End Sub
+End Class
